ad2a3b74f7b88919fae8ae24423c2408a3c12686
priyanshi <priyanshijain078@gmail.com>
2026-07-20 18:06:18 +0530

    initialize Spring Boot project

---
+++ src/main/java/com/priyanshi/event_registration_platform/EventRegistrationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.priyanshi.event_registration_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRegistrationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRegistrationPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyanshi/event_registration_platform/EventRegistrationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanshi.event_registration_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
